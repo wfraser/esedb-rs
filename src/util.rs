bbc16db7c53fd7c6b90ab6cb7c504ae9738535ed
William R. Fraser <wfraser@codewise.org>
2016-08-17 22:44:53 -0700

    comment on the size assert

--- src/util.rs
+++ src/util.rs
@@ -2,7 +2,7 @@ use std::mem::{size_of, transmute};
 use std::slice;
 
 pub unsafe fn slice_transmute<'a, T, U>(src: &'a [T]) -> &'a [U] {
-    assert_eq!(0, src.len() % size_of::<U>());
+    assert_eq!(0, src.len() % size_of::<U>()); // size of U is wrong if it doesn't evenly divide it
     let ptr_of_u: *const U = transmute(src.as_ptr());
     slice::from_raw_parts(ptr_of_u, src.len() / size_of::<U>())
 }
